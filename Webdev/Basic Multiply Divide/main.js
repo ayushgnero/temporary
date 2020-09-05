db5dc5e04cee62bdb2574872b01269393ccd8485
ayushgnero <ayushgautam171099@gmail.com>
2020-09-05 20:52:00 +0530

    ...

---
+++ Webdev/Basic Multiply Divide/main.js
@@ -0,0 +1,13 @@
+function multiplyBy()
+{
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+        document.getElementById("result").innerHTML = num1 * num2;
+}
+
+function divideBy()
+{
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+document.getElementById("result").innerHTML = num1 / num2;
+}
